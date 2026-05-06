f77b0c9e0f90e7e03f9f5b2d46b99dd9c365b66c
Erin <enguy4@ocdsb.ca>
2026-05-05 21:36:36 -0400

    span works :D + let there be info on the first row

--- my-app/app/hi/ald1/meetthemembers/page.tsx
+++ my-app/app/hi/ald1/meetthemembers/page.tsx
@@ -11,7 +11,6 @@ export default function meetmem(){
   const [expanded, setExpanded] = useState(false)
   const [expandedIndex, setExpandedIndex] = useState<number | null>(null)
 
-
   const handleClick = (index: number) => {
     setExpandedIndex(expandedIndex === index ? null : index)
 }
@@ -24,17 +23,52 @@ export default function meetmem(){
     <Image src = "/home.png" alt = "home" height={50} width = {50} className = "absolute top-4 right-4"></Image>
     </Link>
 
-    <h1 className = "absolute top-4 left-4 text-white font-netflix text-5xl font-thin"> Meet the members</h1>
-        <div className = " flex flex-row mb-20 gap-5 ">
-          <Image src  = "/PO1.png" alt = "sangwon" width = {250} height = {350} className = "hover:scale-110 duration-300 cursor-pointer" onClick ={() => handleClick(0)}></Image>
-          <div className = {` overflow-hidden transition-all duration-300 ease-in-out bg-orange-50 text-white flex flex-col rounded-lg h-12 w-0 ${expandedIndex === 0 ? 'w-96' : 'w-0'}` }></div>
-        <Image src = "/PO2.png" alt = "anxin" width = {300} height = {350} className = " cursor-pointer hover:scale-110 duration-300 " onClick = {() => handleClick(1)}></Image>
-          <div className = {` overflow-hidden transition-all duration-300 ease-in-out bg-orange-50 text-white flex flex-col rounded-lg h-12 w-0 ${expandedIndex === 1 ? 'w-96' : 'w-0'}`}></div>
-        <Image src = "/PO3.png" alt = "xinlong" width = {300} height = {350} className = "hover:scale-110 duration-300" onClick = {() => handleClick(2)}></Image>
-          <div className = {` overflow-hidden transition-all duration-300 ease-in-out bg-orange-50 text-white flex flex-col rounded-lg h-12 w-0 ${expandedIndex ===2? 'w-96' : 'w-0'}`}></div>
-        <Image src = "/PO4.png" alt = "geonwoo" width = {300} height = {350} className = "hover:scale-110 duration-300"></Image>
-          <div className = "flex flex-col"></div>
-      </div>
+    <h1 className = "absolute top-6 left-6 text-white font-netflix text-5xl font-thin "> Meet the members</h1>
+    <div className = " flex flex-row mb-20 gap-5 ">
+  <div className={`flex items-center transition-all duration-500 ${expandedIndex !== null && expandedIndex !== 0 ? 'hidden' : 'flex'}`}>
+     <Image src="/PO1.png" onClick={() => handleClick(0)} className="cursor-pointer hover:scale-110 duration-300 z-1" width= {expandedIndex === 0 ? 270: 270} height={350} alt="sangwon"/>
+     <div className={`overflow-hidden bg-orange-50 transition-all duration-500 rounded-xl z-0 ${expandedIndex === 0 ? 'w-260 p-5 h-60' : 'w-0 h-60'}`}>
+        <h1 className = " text-5xl"> Lee Sangwon (이상원)</h1>
+        <p className = "font-netflix mt-2 text-xl">Stage Name: Sangwon <br />Birthday: May 8 2003 <br/>Height: 177cm <br/>MBTI: INFP <br/>Position: Center, Lead Dancer </p>
+     </div>
+  </div>
+<div className={`flex items-center transition-all duration-500 ${expandedIndex !== null && expandedIndex !== 1 ? 'hidden' : 'flex'}`}>
+     <Image src="/PO2.png" onClick={() => handleClick(1)}  className="cursor-pointer transition-all hover:scale-110 duration-300" width={expandedIndex === 0 ? 300: 300} height={350} alt="anxin" />
+     <div className={`overflow-hidden bg-orange-50 transition-all duration-500 ${expandedIndex === 1 ? 'w-250 p-5 h-60' : 'w-0 h-60 '}`}>
+        <h1 className = "text-5xl"> Zhou Anxin (주안신)(周安信)</h1>
+          <div className = "flex justify-between text-xl mt-2">
+            <span>Stage Name: Anxin</span>
+            <span className = "mr-30">Nationality: Chinese</span>
+          </div>
+        <p className = "font-netflix text-xl">Birthday: December 26, 2006 <br/>Height: 178cm <br/>MBTI: ISTP <br/>Position: Main Vocalist</p>
+     </div>
+  </div>
+<div className={`flex items-center transition-all duration-500 ${expandedIndex !== null && expandedIndex !== 2 ? 'hidden' : 'flex'}`}>
+      <Image src="/PO3.png" onClick={() => handleClick(2)}  className="cursor-pointer transition-all hover:scale-110 duration-300" width={expandedIndex === 0 ? 300: 300} height={350} alt="xinlong" />
+          <div className={`overflow-hidden bg-orange-50 transition-all duration-500 ${expandedIndex === 2 ? 'w-260 p-5 h-60' : 'w-0 h-60 '}`}>
+            <h1 className = "text-5xl"> He Xinlong (하흠룡)(贺鑫隆)</h1>
+              <div className = "flex justify-between text-xl mt-2">
+              <span>Stage Name: Xinlong</span>
+            <span className = "mr-30">Nationality: Chinese</span>
+            </div>
+            <p className = "text-xl">Birthday: March 11, 2005 <br/>Height: 177cm <br/>MBTI: INFJ <br/>Position: Main Rapper</p>
+          </div>
+</div>
+
+
+<div className = {`flex items-center transition-all duration-500 ${expandedIndex !== null && expandedIndex !== 3? 'hidden' : 'flex'}`}>
+        <Image src = "/PO4.png" alt = "geonwoo" width = {expandedIndex === 3 ? 310: 310} height = {350} className = "hover:scale-110 duration-300" onClick = {() => handleClick(3)}></Image>
+          <div className = {`overflow-hidden bg-orange-50 transition-all duration-500 ${expandedIndex === 3 ? 'w-250 p-5 h-60' : 'w-0 h-60'}`}> 
+            <h1 className = "text-5xl">Kim Geonwoo (김건우) </h1>
+              <div className = "flex justify-between text-xl mt-2">
+                <span>Stage Name: Geonwoo</span>
+                <span className = "mr-30">Nationality: Korean</span>
+              </div>
+              <p className = "text-xl">Birthday:April 11, 2003 <br/>Height: 184cm <br/>MBTI: ESTJ <br/>Position: Main Vocalist</p>
+            </div>
+  </div>
+</div>
+    
       <div className = "flex flex-row gap-5 -ml-6">
         <Image src = "/PO5.png" alt = "arno" width = {300} height = {350} className = "hover:scale-110 duration-300"></Image>
           <div className = "flex flex-col"></div>
--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -9,8 +9,8 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <main className="flex items-center justify-center h-screen bg-black">
       <Link href = "/hi"
-        className="font-bebas text-9xl text-center tracking-[0.20em] font-medium text-red-600 hover:scale-120 duration-300 cursor-pointer"     >
-        ALD1FLIX
+        className="font-bebas text-9xl text-center tracking-[0.20em] font-medium text-red-600 hover:scale-120 duration-300 cursor-pointer">
+        ALD1FLIX 
       </Link>
     </main>
   )
